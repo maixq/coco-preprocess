c092fb8ba78655a9b3bb8fd319ee83f4f77ef41b
maixq <mai.xueqiao@carro.co>
2023-01-20 16:23:23 +0800

    add panel detector

--- car_detect.py
+++ car_detect.py
@@ -12,6 +12,10 @@ import os
 import time
 
 import matplotlib.pyplot as plt
+from detectron2.utils.visualizer import ColorMode
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Car Detection class
 class CarDetector():
@@ -44,25 +48,61 @@ class CarDetector():
         if len(boxes) == 0:
             return Image.fromarray(img), None
         else:
-            # Crop Image
-            box = list(boxes)[0].detach().cpu().numpy()
-            crop_img, cropped_image = self.crop_object(img, box)
-            
-            # Crop segmentation mask
-            mask = list(masks)[0].detach().cpu().numpy()
-            cropped_mask = self.crop_object(mask, box)
-            
-            return crop_img, cropped_image
-
-    def crop_object(self, image, box):
-        image = Image.fromarray(image)
-        crop_img =  (int(box[0]), int(box[1]), int(box[2]), int(box[3]))
-        cropped_image = image.crop(
-            (int(box[0]), int(box[1]), int(box[2]), int(box[3]))
-            )
-        return crop_img, cropped_image
+            # Crop Imag
+            print(boxes)
+        return 
 # Create CarDetector object
 car_detector = CarDetector(
     config='configs/pointrend_rcnn_R_50_FPN_3x_coco.yaml',
     checkpoint='models/car_detector_1.pth'
-) 
+)
+
+# Detect panels
+
+class PanelDetector():
+    def __init__(self, panel_config, panel_checkpoint, panel_threshold=0.88, model_device='cpu'):
+        self.panel_model = None
+        self.panel_classes = None
+        self.panel_class_metadata = None
+        self.panel_config = panel_config
+        self.panel_checkpoint = panel_checkpoint
+        self.panel_threshold = panel_threshold
+        self.model_device = model_device
+        self.initialise_panel()
+
+    def initialise_panel(self):
+
+        cfg = get_cfg()
+        add_pointrend_config(cfg)
+        cfg.merge_from_file(self.panel_config)
+        cfg.MODEL.WEIGHTS = self.panel_checkpoint
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = self.panel_threshold
+        cfg.MODEL.DEVICE = self.model_device
+        self.panel_model = DefaultPredictor(cfg)
+        logger.info('Panel Model Loaded')
+    
+    def predict(self, img):
+        '''
+        Function that takes in a Numpy Array and country code and returns an Masked Image or None
+        '''
+        t1 = time.time()
+        predictions = self.panel_model(img)
+        t2 = time.time()
+        logger.info('Panel Detection Model Inference took: {}'.format(str(t2 - t1)))
+
+        instances = predictions["instances"].to("cpu")
+
+        # Get detection boxes
+        boxes = instances.pred_boxes.tensor.cpu().numpy()
+        pred_classes = instances.pred_classes.cpu().numpy()
+        # If no boxes are detected, return original image and None for mask
+        if len(boxes) == 0:
+            return Image.fromarray(img), None
+        else:
+            # Crop Image
+            return boxes, pred_classes
+
+panel_detector = PanelDetector(
+    panel_config='configs/pointrend_rcnn_X_101_32x8d_FPN_3x_coco.yaml',
+    panel_checkpoint='/Users/maixueqiao/Downloads/Project/damage_localisation/models/panel_detector.pth',
+    )
